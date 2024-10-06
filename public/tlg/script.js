5b5c423bf8d77c95c45a86f124d76ad3d6b51794
Ihor Petrov <petrov120482@gmail.com>
2024-10-06 19:55:10 +0200

    added cicd env 36

--- public/tlg/script.js
+++ public/tlg/script.js
@@ -13,7 +13,7 @@ document.getElementById('currency-form').addEventListener('submit', function(eve
     fetch(`https://endpointtools.com/api/currency/convert?fromCurrency=${fromCurrency}&toCurrency=${toCurrency}&amount=${amount}`, {
         method: 'GET',
         headers: {
-            'Authorization': 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJpYXQiOjE3MjgyMzU0NzQsImV4cCI6MTcyODIzOTA3NCwicm9sZXMiOlsiUk9MRV9BRE1JTiIsIlJPTEVfVVNFUiJdLCJ1c2VybmFtZSI6Im1haWwxQGV4YW1wbGUuY29tIn0.LOsKrkxNJC1AlpyQaxhqWeaoSxXuczXjzZkbm19jrIrRNWx9lRgEZgs2slUtpBC8V_szkxwCu-C3nKoyX6UwDg5Gbdg8y7k0480hUzYWxHo7HN-06DqJT-dd-R9kSzSPRZlR5tiy4kNmtyfMHxE4NYNfIKRf8x30OTzlugwd0Czu4g_lXmOtHKpE6sH-8gl4w19tuAy1LXv0Q48_5C1DOjhbKnJkAhF6B0tbx5Xb3YjMuF7kOLOxTDEdeiJkGI8X1emSiR3hJUPPaueQq9PV2YD3GzgLo5p225yoL-FDo463c1NyZ4AoBsLXP6cdzOMI3SoOKF5_91yCF0lZIbqh2A',
+            'Authorization': `Bearer ${apiToken}`,
             'accept': 'application/json'
         }
     })
