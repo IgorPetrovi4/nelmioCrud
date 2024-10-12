532d50b76f18072ba416dab9f9728bb028f0b8b1
Ihor Petrov <petrov120482@gmail.com>
2024-10-12 09:43:45 +0200

    test change

--- src/Controller/Telegram/TelegramController.php
+++ src/Controller/Telegram/TelegramController.php
@@ -8,6 +8,7 @@ use App\ApiClient\Interface\TelegramClientInterface;
 use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TelegramController extends AbstractController
@@ -19,23 +20,30 @@ class TelegramController extends AbstractController
     ) {}
 
     #[Route('/send-webapp-link', name: 'send_webapp_link', methods: ['GET'])]
-    public function sendWebAppLink(): JsonResponse
+    public function sendWebAppLink(): RedirectResponse
     {
-        $chatId = 5485716556; // Ваш chat_id
-        $webAppUrl = 'https://endpointtools.com/webapp'; // Ссылка на маршрут Symfony
-        $this->logger->info('Request to API', ['chat_id' => $chatId]);
-        $this->logger->info('WebApp URL', ['url' => $webAppUrl]);
-        $keyboard = [
-            [
-                [
-                    'text' => 'Открыть калькулятор зарплаты',
-                    'web_app' => ['url' => $webAppUrl]
-                ]
-            ]
-        ];
 
-        $this->telegramClient->sendMessage($chatId, 'Нажмите на кнопку для расчета зарплаты:', $keyboard);
+        $webAppUrl = 'https://endpointtools.com/webapp';
+        $botUsername = '@myTestAppWebIhor_bot';
+        // Create a deep link URL
+        $deepLinkUrl = sprintf('https://t.me/%s?start=%s', $botUsername, urlencode($webAppUrl));
 
-        return new JsonResponse(['status' => 'success']);
+        // Redirect to the deep link URL
+        return $this->redirect($deepLinkUrl);
+//        $chatId = 5485716556; // Ваш chat_id
+//        $this->logger->info('Request to API', ['chat_id' => $chatId]);
+//        $this->logger->info('WebApp URL', ['url' => $webAppUrl]);
+//        $keyboard = [
+//            [
+//                [
+//                    'text' => 'Открыть калькулятор зарплаты',
+//                    'web_app' => ['url' => $webAppUrl]
+//                ]
+//            ]
+//        ];
+//
+//        $this->telegramClient->sendMessage($chatId, 'Нажмите на кнопку для расчета зарплаты:', $keyboard);
+//
+//        return new JsonResponse(['status' => 'success']);
     }
 }
